faec22df950db551272ebce5b8eaaa4261e651a7
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-10-04 09:10:34 +0200

    playing with rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, take, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rcxjguide';
-  someVariable:any
+  count:number=0
+
+  ngOnInit():void{
+    const foo = new Observable((subscriber) => {
+      subscriber.next(42);
+      subscriber.next(100); // "return" another value
+      subscriber.next(200); // "return" yet another
+    });
+
+    console.log('before');
+    foo.pipe(take(3)).subscribe({
+      next:(x)=>{
+        console.log(x)
+      },
+      complete:()=>{
+        console.log('after')
+      }
+    });
+  }
+
 }
